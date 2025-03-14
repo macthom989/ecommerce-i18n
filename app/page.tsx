5233f6c6c2814bc2f064979e5f94f7aa0d6ec98a
hoangvu <hoangvucgame@gmail.com>
2025-03-14 12:00:26 +0700

    feat: Ãadd slide

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,122 @@
-import { Header } from '@/components/header';
-import { BannerGrid } from '@/components/banner-grid';
+import BannerBlock from '@/components/banner/banner-block';
 import { Footer } from '@/components/footer';
+import { Header } from '@/components/header';
 
 export default async function Home() {
+  const homeThreeMasonryBanner = [
+    {
+      id: 1,
+      title: "Men's Collection",
+      slug: 'mens-collection',
+      image: {
+        mobile: {
+          url: '/assets/images/banner/masonry/banner-mobile-1.jpg',
+          width: 470,
+          height: 232,
+        },
+        desktop: {
+          url: '/assets/images/banner/masonry/banner-1.jpg',
+          width: 1078,
+          height: 425,
+        },
+      },
+      type: 'medium',
+    },
+    {
+      id: 2,
+      title: 'New Sports',
+      slug: 'new-sports',
+      image: {
+        mobile: {
+          url: '/assets/images/banner/masonry/banner-mobile-2.jpg',
+          width: 232,
+          height: 232,
+        },
+        desktop: {
+          url: '/assets/images/banner/masonry/banner-2.jpg',
+          width: 425,
+          height: 425,
+        },
+      },
+      type: 'small',
+    },
+    {
+      id: 3,
+      title: 'Dress Women',
+      slug: 'dress-women',
+      image: {
+        mobile: {
+          url: '/assets/images/banner/masonry/banner-mobile-3.jpg',
+          width: 232,
+          height: 232,
+        },
+        desktop: {
+          url: '/assets/images/banner/masonry/banner-3.jpg',
+          width: 425,
+          height: 425,
+        },
+      },
+      type: 'small',
+    },
+    {
+      id: 4,
+      title: 'Exclusive Sunglasses',
+      slug: 'exclusive-sunglasses',
+      image: {
+        mobile: {
+          url: '/assets/images/banner/masonry/banner-mobile-4.jpg',
+          width: 232,
+          height: 232,
+        },
+        desktop: {
+          url: '/assets/images/banner/masonry/banner-4.jpg',
+          width: 425,
+          height: 425,
+        },
+      },
+      type: 'small',
+    },
+    {
+      id: 5,
+      title: 'Product Coupons',
+      slug: 'product-coupons',
+      image: {
+        mobile: {
+          url: '/assets/images/banner/masonry/banner-mobile-5.jpg',
+          width: 232,
+          height: 232,
+        },
+        desktop: {
+          url: '/assets/images/banner/masonry/banner-5.jpg',
+          width: 425,
+          height: 425,
+        },
+      },
+      type: 'small',
+    },
+    {
+      id: 6,
+      title: 'New Backpack',
+      slug: 'new-backpack',
+      image: {
+        mobile: {
+          url: '/assets/images/banner/masonry/banner-mobile-6.jpg',
+          width: 470,
+          height: 232,
+        },
+        desktop: {
+          url: '/assets/images/banner/masonry/banner-6.jpg',
+          width: 1078,
+          height: 425,
+        },
+      },
+      type: 'medium',
+    },
+  ];
   return (
     <>
       <Header />
-      <main className={'relative flex-grow'}>
-        <BannerGrid />
-      </main>
+      <BannerBlock data={homeThreeMasonryBanner} />
       <Footer />
     </>
   );
--- components/banner-grid.tsx
+++ components/banner-grid.tsx
@@ -11,7 +11,7 @@ export function BannerGrid() {
   ];
 
   return (
-    <div className="mb-12 md:mb-14 xl:mb-16 px-2.5 grid grid-cols-2 sm:grid-cols-9 gap-2 md:gap-2.5 max-w-[1440px] mx-auto">
+    <div className="bg-rose-950 text-teal-400 p-8 mb-12  md:mb-14 xl:mb-16 px-2.5 grid grid-cols-2 sm:grid-cols-9 gap-2 md:gap-2.5 max-w-[1440px] mx-auto">
       {banners.map((banner, index) => (
         <div key={index} className={`relative w-full ${banner.span}`}>
           <Image
